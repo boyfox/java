001eb842abadb8c81cda3262e6f03fb65cc2c031
boyfox <boyfox@boyfox-PC>
2014-02-22 23:51:21 +0500

    ByteUtil for nio service

---
+++ src/com/smsoft/nio/util/ByteUtil.java
@@ -0,0 +1,23 @@
+package com.smsoft.nio.util;
+
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
+public class ByteUtil {
+
+	public byte[] intToByteArray(int i) {
+		return ByteBuffer.allocate(4).putInt(i).array();
+	}
+	
+	public int byteArrayToInt(byte[] byteArray) throws BufferUnderflowException {
+		return ByteBuffer.wrap(byteArray).getInt();
+	}
+	
+	public byte[] longToByteArray(long l) {
+		return ByteBuffer.allocate(8).putLong(l).array();
+	}
+	
+	public long byteArrayToLong(byte[] byteArray) throws BufferUnderflowException {
+		return ByteBuffer.wrap(byteArray).getLong();
+	}
+}
